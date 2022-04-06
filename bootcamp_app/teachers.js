91b517353f9745ab60e51ca7af112caa75a72a71
robotsch <christianhumble22@gmail.com>
2022-04-06 18:21:27 -0400

    lint code

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -2,17 +2,19 @@ const { Pool } = require('pg');
 
 const pool = new Pool({database: 'bootcampx'})
 
+const values = [`${process.argv[2] || 'JUL02'}`]
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(row => {
-    console.log(`${process.argv[2]}: ${row.teacher}`);
+    console.log(`${values[0]}: ${row.teacher}`);
   })
 }).catch(err => console.error('query error', err.stack));
